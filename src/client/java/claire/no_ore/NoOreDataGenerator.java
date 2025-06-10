12b5ada2ace2ad91fadd18d63142091ef53f32b4
ameriturk <ummmratio@gmail.com>
2025-06-10 06:41:21 -0400

    Add files via upload

---
+++ src/client/java/claire/no_ore/NoOreDataGenerator.java
@@ -0,0 +1,11 @@
+package claire.no_ore;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NoOreDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
